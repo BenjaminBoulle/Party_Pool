15fa1aafbe8d6739d170d1d62fd580fd70ea9fb9
Benjamin Boulle <benjaminboulle.sjb@gmail.com>
2022-11-21 16:40:29 +0100

    created the models

---
+++ db/migrate/20221121153638_create_locations.rb
@@ -0,0 +1,12 @@
+class CreateLocations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :locations do |t|
+      t.string :title
+      t.string :address
+      t.string :category
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
