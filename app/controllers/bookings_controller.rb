d807a8f36df1d884827ea1ec6f04848fa63b1d48
Gideon Maydell <gideon@maydell.at>
2022-11-22 16:46:51 +0100

    worked on the bookings controller and view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,26 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @location = Location.find(params[:location_id])
+  end
+
+  def create
+    @booking = Booking.new(bookings_params)
+    # booking user is the current_user
+    @booking.user = current_user
+    # booking location is the location from the params[:location_id]
+    @location = Location.find(params[:location_id])
+    @booking.location = @location
+    if @booking.save
+      redirect_to location_path(@booking.location)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def bookings_params
+    params.require(:booking).permit(:start_date, :end_date, :remark)
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :location
 
-  validates :start_date, :end_date, uniqueness: true
   validates :start_date, :end_date, presence: true
 end
