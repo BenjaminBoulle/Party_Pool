d618326a2a272927176cc3b1131092f8699d8a3c
Benjamin Boulle <benjaminboulle.sjb@gmail.com>
2022-11-22 12:49:06 +0100

    intermediate commit for the location branch

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,45 @@
 class LocationsController < ApplicationController
+  def index
+    @locations = Location.all
+  end
+
+  def show
+    @location = Location.find(params[:id])
+    @owner = @location.user.email.split('@').first
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      redirect_to location_path(@location)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @location = Location.find(params[:id])
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    @location.update(location_params)
+    redirect_to locartions_path()
+  end
+
+  def destroy
+    @location = Location.find(params[:id])
+    @location.destroy
+    redirect_to locations_path, status: :see_other
+  end
+
+  private
+
+  def location_params
+    params.require(:restaurant).permit(:title, :address, :category)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+CATEGORY = "'kids party', 'student party', 'wedding', 'bal', 'birthday'"
+
+Location.create(user_id: 1, title: 'Le Hangar', address: 'Avenue Champelle 1, Rhode-Saint_genèse', category: CATEGORY)
+Location.create(user_id: 1, title: 'Factory', address: 'Cantersteen 12, 1000 Bruxelles', category: CATEGORY)
+Location.create(user_id: 1, title: "L'Usine", address: 'Pl. de la Vieille Halle aux Blés 28, 1000 Bruxelles', category: CATEGORY)
+Location.create(user_id: 1, title: 'Wood Cutter', address: 'Rue des Alexiens 37, 1000 Bruxelles', category: CATEGORY)
+Location.create(user_id: 1, title: 'Beach', address: 'Rue de la Violette 22, 1000 Bruxelles', category: CATEGORY)
+Location.create(user_id: 1, title: 'The Penthouse', address: 'Rijswijkseplein 786, 2516 LX Den Haag, Netherlands', category: CATEGORY)
+Location.create(user_id: 1, title: 'The Palace', address: 'Avenue du Monoplan 12, 1150 Bruxelles', category: CATEGORY)
