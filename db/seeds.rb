952f0b28b3585387be83ded38a541a849d86b24b
Gideon Maydell <gideon@maydell.at>
2022-11-23 12:58:54 +0100

    I worked on the bookings function

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+Booking.destroy_all
 Location.destroy_all
 User.destroy_all
 CATEGORY = "'kids party', 'student party', 'wedding', 'ball', 'birthday'"
